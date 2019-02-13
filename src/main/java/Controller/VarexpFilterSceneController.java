88cb4da67d534fae7492e5e75dce4bd10a2077ec
Stephen <cheunste@gmail.com>
2019-02-13 14:36:35 -0800

    Initial commit of the VarexpHeaderFilter Scene and its controller

--- src/main/java/Controller/VarexpFilterSceneController.java
+++ src/main/java/Controller/VarexpFilterSceneController.java
@@ -1,4 +1,30 @@
 package Controller;
 
-public class VarexpFilterSceneController {
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VarexpFilterSceneController implements Initializable {
+
+
+    @FXML
+    private JFXButton saveBtn;
+    @FXML
+    private JFXButton cancelBtn;
+    @FXML
+    private JFXButton clearBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        saveBtn.setOnAction(e -> {
+        });
+        cancelBtn.setOnAction(e -> {
+        });
+        clearBtn.setOnAction(e -> {
+        });
+    }
 }
