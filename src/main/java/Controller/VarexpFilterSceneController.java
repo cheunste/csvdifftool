7abd8a9295952dcaff1b1bf943f10e9d51fbaf60
Stephen <cheunste@gmail.com>
2019-02-18 11:27:32 -0800

    Updated the VarexpFilterScene and its controller

--- src/main/java/Controller/VarexpFilterSceneController.java
+++ src/main/java/Controller/VarexpFilterSceneController.java
@@ -2,12 +2,15 @@ package Controller;
 
 import VarexpInterface.pcvue.fields.VarexpFactory;
 import VarexpInterface.pcvue.fields.VarexpTuple;
+import VarexpInterface.pcvue.fields.VarexpVariable;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
@@ -34,6 +37,27 @@ public class VarexpFilterSceneController implements Initializable {
     private JFXButton cancelBtn;
     @FXML
     private JFXButton clearBtn;
+
+    //Member variables. All private
+    private final int COLUMN_NUM = 4;
+    private final int ITEMS_PER_COLUMN = VarexpVariable.getFieldNum() / COLUMN_NUM;
+    private final int FIELD_NUM_INDEX = 1;
+    private final int DESCRIPTION_INDEX = 2;
+    private final int CHECK_BOX_INDEX = 3;
+    //Grid panes
+    @FXML
+    private GridPane col1;
+    @FXML
+    private GridPane col2;
+    @FXML
+    private GridPane col3;
+    @FXML
+    private GridPane col4;
+    //Scroll Pane
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private HBox columnContainer;
 
     /**
      * This method initializes the GUI  by setting up button actions, event listeners, etc.
@@ -84,10 +108,18 @@ public class VarexpFilterSceneController implements Initializable {
 
 
         /*
+        TODO: ScrollPane
+         */
+        scrollPane.setContent(columnContainer);
+
+        /*
          * Create a varexp factory,
          * Use it get generate all the fields and have said field call the getFieldMap()
          */
         VarexpFactory factory = new VarexpFactory();
+
+        int columnCount = 0;
+        int temp = 0;
 
         for (String variable : factory.listOfTables) {
             Map<String, VarexpTuple> variableFieldMap = factory.declareNewVariable(variable).getFieldMap();
@@ -95,24 +127,53 @@ public class VarexpFilterSceneController implements Initializable {
             for (String key : variableFieldMap.keySet()) {
                 //Get the position
                 int position = (Integer) variableFieldMap.get(key).getPosition();
-                HBox filterHBox = new HBox();
 
                 Label fieldNum = new Label(Integer.toString(position));
                 fieldNum.setTextAlignment(TextAlignment.LEFT);
 
                 Label fieldDescription = new Label(key);
+                fieldDescription.setTextAlignment(TextAlignment.CENTER);
 
                 JFXCheckBox filterCheckbox = new JFXCheckBox();
-                filterCheckbox.setAlignment(Pos.CENTER);
+                filterCheckbox.setAlignment(Pos.CENTER_LEFT);
 
-                //Add all the above widgets into the HBox
-                filterHBox.getChildren().addAll(fieldNum, fieldDescription, filterCheckbox);
-                //Add the Hbox onto the GridPane
+                //Add all the above widgets into the VBOXes
+                if (columnCount <= ITEMS_PER_COLUMN) {
+
+                    System.out.println("Column 1: " + columnCount + " : " + key);
+                    //col1.add(filterHBox,1,columnCount);
+                    col1.add(fieldNum, FIELD_NUM_INDEX, columnCount);
+                    col1.add(fieldDescription, DESCRIPTION_INDEX, columnCount);
+                    col1.add(filterCheckbox, CHECK_BOX_INDEX, columnCount);
+                } else if (columnCount <= (2 * ITEMS_PER_COLUMN) &&
+                        columnCount > ITEMS_PER_COLUMN
+
+                ) {
+                    System.out.println("Column 2: " + columnCount + " : " + (columnCount - ITEMS_PER_COLUMN) + " : " + key);
+
+                    //col2.add(filterHBox,1,0);
+                    col2.add(fieldNum, FIELD_NUM_INDEX, columnCount - (ITEMS_PER_COLUMN));
+                    col2.add(fieldDescription, DESCRIPTION_INDEX, columnCount - (ITEMS_PER_COLUMN));
+                    col2.add(filterCheckbox, CHECK_BOX_INDEX, columnCount - (ITEMS_PER_COLUMN));
+                } else if (columnCount <= (3 * ITEMS_PER_COLUMN) &&
+                        columnCount > (2 * ITEMS_PER_COLUMN)
+                ) {
+                    System.out.println("Column 3: " + columnCount + " : " + (columnCount - (2 * ITEMS_PER_COLUMN)) + " : " + key);
+                    //col3.add(filterHBox,1,0);
+
+                    col3.add(fieldNum, FIELD_NUM_INDEX, columnCount - (2 * ITEMS_PER_COLUMN));
+                    col3.add(fieldDescription, DESCRIPTION_INDEX, columnCount - (2 * ITEMS_PER_COLUMN));
+                    col3.add(filterCheckbox, CHECK_BOX_INDEX, columnCount - (2 * ITEMS_PER_COLUMN));
+                } else {
+                    System.out.println("Column 4: " + columnCount + " : " + (columnCount - (3 * ITEMS_PER_COLUMN)) + " : " + key);
+                    //col4.add(filterHBox,1,0);
+                    col4.add(fieldNum, FIELD_NUM_INDEX, columnCount - (3 * ITEMS_PER_COLUMN));
+                    col4.add(fieldDescription, DESCRIPTION_INDEX, columnCount - (3 * ITEMS_PER_COLUMN));
+                    col4.add(filterCheckbox, CHECK_BOX_INDEX, columnCount - (3 * ITEMS_PER_COLUMN));
+                }
+                columnCount++;
             }
-
         }
-
-
     }
 
     //This is used to keep track of prvevious stages so I can close them later
